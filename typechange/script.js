20217daf16f4667d9d3941da6bf25d5e2aeb56f1
jfengsky <jfengsky@gmail.com>
2014-04-14 09:56:28 +0800

    add typechange

---
+++ typechange/script.js
@@ -0,0 +1,14 @@
+/**
+ * Description:
+ * Author: jiangfeng(jiang.f@ctrip.com)
+ * Date: 2014-04-14 09:27
+ *
+ */
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById('demo').setAttribute('type', 'date')
+}, false);
+
+//document.getElementById('changeClick').addEventListener('click', function(e){
+//  document.getElementById('demo').setAttribute('type', 'date')
+//}, false)
