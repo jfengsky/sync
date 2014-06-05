084fea41cb7b73bcdb7d60f77e3cd91f3ef1d689
jfengsky <jfengsky@gmail.com>
2014-06-05 19:42:52 +0800

    countdown modify

--- trip/temai/mod_timedown.js
+++ trip/temai/mod_timedown.js
@@ -18,38 +18,38 @@ define(function(require, exports, module) {
       $(_obj).html(_time.d + '天' + _time.h + '小时' + _time.m + '分' + _time.s + '秒');
     };
 
-    this._timeRender = function(_Obj, _interCallback, _endCallback){
-      var tempTime = $(_Obj).attr('data-time'),
-          timeArr = [];
-      if(tempTime){
-        timeArr = tempTime.split('|');
-        countDown.init({
-          startTime: timeArr[0],
-          endTime: timeArr[1],
-          intervalback: function(_data){
-            if(_interCallback){
-              _interCallback(_Obj, _data);
-            }
-          },
-          callback: function(){
-            if(_endCallback){
-              _endCallback();
-            }
-          }
-        });
-      }
-    };
+//    this._timeRender = function(_Obj, _interCallback, _endCallback){
+//      var tempTime = $(_Obj).attr('data-time'),
+//          timeArr = [];
+//      if(tempTime){
+//        timeArr = tempTime.split('|');
+//        countDown.init({
+//          startTime: timeArr[0],
+//          endTime: timeArr[1],
+//          intervalback: function(_data){
+//            if(_interCallback){
+//              _interCallback(_Obj, _data);
+//            }
+//          },
+//          callback: function(){
+//            if(_endCallback){
+//              _endCallback();
+//            }
+//          }
+//        });
+//      }
+//    };
 
     /**
      * 顶部倒计时
      */
-    this._topCD = function(){
-      var tempTime = $('#J_topcd').attr('data-time'),
-          timeArr = [];
-      if(tempTime){
-//        timeArr = tempTime.split('|');
-      }
-    },
+//    this._topCD = function(){
+//      var tempTime = $('#J_topcd').attr('data-time'),
+//          timeArr = [];
+//      if(tempTime){
+////        timeArr = tempTime.split('|');
+//      }
+//    },
 
     /**
      * 初始化
@@ -57,22 +57,22 @@ define(function(require, exports, module) {
      */
     this.init = function(){
       $.each($('.J_count'), function(_index, _item){
-        self._timeRender(_item, self._interval);
-//        var tempTime = $(_item).attr('data-time'),
-//          timeArr = [];
-//        if(tempTime){
-//          timeArr = tempTime.split('|');
-//          countDown.init({
-//            startTime: timeArr[0],
-//            endTime: timeArr[1],
-//            intervalback: function(_data){
-//              self._interval(_item, _data);
-//            },
-//            callback: function(){
-//              console.log('end');
-//            }
-//          });
-//        }
+//        self._timeRender(_item, self._interval);
+        var tempTime = $(_item).attr('data-time'),
+          timeArr = [];
+        if(tempTime){
+          timeArr = tempTime.split('|');
+          countDown.init({
+            startTime: timeArr[0],
+            endTime: timeArr[1],
+            intervalback: function(_data){
+              self._interval(_item, _data);
+            },
+            callback: function(){
+              console.log('end');
+            }
+          });
+        }
       });
     };
   };
