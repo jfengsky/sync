d8a6e9e61590744c1e2c8d8067f101d429773fef
jfengsky <jfengsky@gmail.com>
2014-06-05 19:37:34 +0800

    countdown modify

--- trip/temai/main.js
+++ trip/temai/main.js
@@ -8,5 +8,12 @@ define(function (require) {
   var countdown = require('./mod_timedown');
   new countdown().init();
 
+  // TODO tab切换
+
+  // TODO 产品筛选
+
+  // TODO 产品排序
+
+  // TODO 翻页
 
 });
--- trip/temai/mod_timedown.js
+++ trip/temai/mod_timedown.js
@@ -17,28 +17,62 @@ define(function(require, exports, module) {
     this._interval = function(_obj, _time){
       $(_obj).html(_time.d + '天' + _time.h + '小时' + _time.m + '分' + _time.s + '秒');
     };
-    
+
+    this._timeRender = function(_Obj, _interCallback, _endCallback){
+      var tempTime = $(_Obj).attr('data-time'),
+          timeArr = [];
+      if(tempTime){
+        timeArr = tempTime.split('|');
+        countDown.init({
+          startTime: timeArr[0],
+          endTime: timeArr[1],
+          intervalback: function(_data){
+            if(_interCallback){
+              _interCallback(_Obj, _data);
+            }
+          },
+          callback: function(){
+            if(_endCallback){
+              _endCallback();
+            }
+          }
+        });
+      }
+    };
+
+    /**
+     * 顶部倒计时
+     */
+    this._topCD = function(){
+      var tempTime = $('#J_topcd').attr('data-time'),
+          timeArr = [];
+      if(tempTime){
+//        timeArr = tempTime.split('|');
+      }
+    },
+
     /**
      * 初始化
      * @return
      */
     this.init = function(){
       $.each($('.J_count'), function(_index, _item){
-        var tempTime = $(_item).attr('data-time'),
-          timeArr = [];
-        if(tempTime){
-          timeArr = tempTime.split('|');
-          countDown.init({
-            startTime: timeArr[0],
-            endTime: timeArr[1],
-            intervalback: function(_data){
-              self._interval(_item, _data);
-            },
-            callback: function(){
-              console.log('end');
-            }
-          });
-        }
+        self._timeRender(_item, self._interval);
+//        var tempTime = $(_item).attr('data-time'),
+//          timeArr = [];
+//        if(tempTime){
+//          timeArr = tempTime.split('|');
+//          countDown.init({
+//            startTime: timeArr[0],
+//            endTime: timeArr[1],
+//            intervalback: function(_data){
+//              self._interval(_item, _data);
+//            },
+//            callback: function(){
+//              console.log('end');
+//            }
+//          });
+//        }
       });
     };
   };
