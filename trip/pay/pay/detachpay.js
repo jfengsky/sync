75491122b40f413f3d7f49be0c68a8445e67760b
jfengsky <jfengsky@gmail.com>
2014-07-16 16:26:22 +0800

    updata pl

---
+++ trip/pay/pay/detachpay.js
@@ -0,0 +1,230 @@
+/**
+ * Description:支付拆分页面
+ * Author: jiangfeng(jiang.f@ctrip.com)
+ * Date: 2014-07-15 13:51
+ *
+ */
+/**
+ * 支付拆分页面
+ * TODO loading动画，添加新支付方式渐现显示
+ * TODO 拆分笔数:后端给我还是根据成人数来判断 （如果成人数为0，那么可拆分笔数=1，即只允许单笔支付）
+ * TODO 拆分按钮显示隐藏
+ * TODO 获取可支持的支付方式数据和显示顺序 （如果当前支付款支持多个支付方式，那么选项显示排序为：信用卡>外网自助支付>现金。默认值为第一个支付方式)
+ * TODO 默认选择哪种支付方式
+ * TODO 现金支付方式只能一个
+ * TODO 	默认显示一条支付款项，支付金额为订单全款。
+ * TODO 	如果当前支付笔数＜可拆分笔数，那么下方有“+支付方式”的按钮，如果当前支付笔数=可拆分笔数，那么不显示“＋支付方式”按钮。
+ *
+ */
+define(function(require, exports, module) {
+  "use strict";
+  var $ = require('jquery');
+
+  // TODO 获取可拆分次数
+  function payDeatch(){
+    var self = this,
+
+      // TODO 获取可拆分次数
+      detachNum = 5,
+
+      // 已经创立的支付方式个数
+      detachIndex = 1,
+
+      radioIndex = 1,
+
+      // TODO 获取订单金额
+      payTotal = 123456,
+
+    // TODO 获取可选支付方式
+    payType = ['信用卡', '外网自助支付', '现金'];
+
+    /**
+     * 可拆分笔数提示
+     * @param {Number} _number
+     * @returns {string}
+     */
+    this._topNotice = function( _number ){
+      return '<li>拆分支付笔数不得大于出行成人数，此订单最多可拆分' + _number + '笔支付</li>';
+    };
+
+    /**
+     * 剩余金额模板
+     * @private
+     */
+    this._leftPay = function(_hasPay, _leftPay, _bool){
+      var btn = '';
+      if(_bool){
+        btn = self._detachButton
+      }
+      return '<li id="J_payinfo">'+ btn +'已选择支付：<span class="pay_price"><dfn>￥</dfn><span id="J_haspay">' + _hasPay + '</span></span>还剩余：<span class="pay_price"><dfn>￥</dfn><span id="J_leftpay">' + _leftPay + '</span></span></li>';
+    }
+
+
+    /**
+     * 拆分按钮 异步加入页面中
+     */
+    this._detachButton = '<a href="javascript:void(0)" id="J_detach" class="pay_waysbtn"><span>+</span>支付方式</a>';
+
+
+    this._listNo = function( _num ){
+      switch (_num){
+        case 1:
+          return '一';
+          break;
+        case 2:
+          return '二';
+          break;
+        case 3:
+          return '三';
+          break;
+        case 4:
+          return '四';
+          break;
+        case 5:
+          return '五';
+          break;
+        case 6:
+          return '六';
+          break;
+        case 7:
+          return '七';
+          break;
+        case 8:
+          return '八';
+          break;
+        case 9:
+          return '九';
+          break;
+        case 10:
+          return '十';
+          break;
+      }
+    };
+
+    /**
+     * 拆分模板
+     * @param {Object} _args 传入参数对象
+     *    NO: 支付序号
+     *    type: 支付方式数组
+     *    total: 支付金额
+     *    hasCash: 是否现金, 只能选一次现金，其它则灰掉
+     */
+    this._tpl = function( _args ){
+      var payTypeList = '';
+      $.each(_args.payType, function(_index, _item){
+        if(_index === 0){
+          payTypeList += '<label class="base_label"><input type="radio" name="pay'+ _args.radioIndex +'" checked>' + _item + '</label>';
+        } else {
+          payTypeList += '<label class="base_label"><input type="radio" name="pay'+ _args.radioIndex +'">' + _item + '</label>';
+        }
+      });
+      return '<li id="J_paylist' + _args.NO + '">' +
+        '<a href="javascript:void(0)" class="pay_del J_patydel">删除</a>' +
+        '<span class="pay_ways">支付方式<span class="J_payindex"></span>：</span>' + payTypeList +
+        '<strong>支付</strong><input type="number" value="' + _args.total + '" class="pay_inputnum J_payinput">元' +
+        '<p class="pay_tipnum" style="display:none"><b></b><i></i>填写金额超出支付金额</p>';
+    };
+
+    /**
+     * 重排支付方式序号
+     * @private
+     */
+    this._payIndex = function(){
+      $.each($('#J_paycnt .J_payindex'), function(_index, _item){
+        $(_item).text(self._listNo(_index + 1));
+      });
+    };
+
+    /**
+     * 按钮绑定添加事件
+     * @private
+     */
+    this._bind = function(){
+
+      // 添加支付方式
+      $('#J_paybox').delegate('#J_detach', 'click', function(){
+        var html = self._tpl({
+          payType: payType,
+          NO: detachIndex,
+          total: payTotal,
+          hasCash: true,
+          detachs: detachNum,
+          radioIndex: radioIndex
+        });
+        $('#J_payinfo').before(html);
+
+        if( detachIndex === detachNum){
+          $('#J_detach').remove();
+        }
+        self._payIndex();
+        detachIndex++;
+        radioIndex++;
+      });
+
+      // 删除支付方式按钮
+      $('#J_paybox').delegate('.J_patydel', 'click', function(){
+
+        if( detachIndex <= 2){
+          // 只剩1个支付方式则删除金额
+
+        } else {
+          $(this).closest('li').remove();
+          // TODO 重写支付金额
+
+          // TODO 重排支付方式序号
+
+          // 加入支付按钮
+          if( $('#J_detach').length === 0){
+            $('#J_payinfo').prepend(self._detachButton);
+          }
+          self._payIndex();
+          detachIndex--;
+        }
+
+      })
+    };
+
+    this._delPay = function(){
+
+    };
+    /**
+     * 默认第一条支付 填写金额 选择第一个支付方式
+     * @private
+     */
+    this._defaultPay = function( _arg ){
+      var html = self._topNotice(_arg.detachs),
+          moreDetach = false;
+      html += self._tpl(_arg);
+      if(_arg.detachs > 1){
+        moreDetach = true
+      }
+      html += self._leftPay(payTotal, 0, moreDetach);
+
+      $('#J_loading').remove();
+      $('#J_paycnt').append(html);
+      self._payIndex();
+      detachIndex++;
+      radioIndex++;
+    };
+
+    // 初始化
+    this.init = function(){
+
+      // 初始化默认拆分模板
+      self._defaultPay({
+        payType: payType,
+        NO: detachIndex,
+        total:payTotal,
+        hasCash: true,
+        detachs: detachNum,
+        radioIndex: radioIndex
+      });
+
+      // 按钮绑定事件
+      self._bind();
+
+    }
+  }
+
+  new payDeatch().init();
+});
---
+++ trip/pay/pay/mod_moneyinput.js
@@ -0,0 +1,14 @@
+/**
+ * Description:表单只能输入金额
+ * Author: jiangfeng(jiang.f@ctrip.com)
+ * Date: 2014-07-16 13:51
+ *
+ */
+
+define(function(require, exports, module) {
+  "use strict";
+  var $ = require('jquery');
+
+  // module.exports = ;
+
+});
