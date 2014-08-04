fa1685c5acf8948656f8526708e9bc8eec136832
jfengsky <jfengsky@gmail.com>
2014-08-04 20:32:31 +0800

    pay modify

--- trip/pay/pay/paycheck.js
+++ trip/pay/pay/paycheck.js
@@ -15,7 +15,24 @@
 define(function(require, exports, module) {
   "use strict";
   var $ = require('jquery'),
-      GVO = GV.app.order;
+      GVO = GV.app.order,
+      PAYTYPES = [{  // 支付方式和显示顺序对照
+        rk: 0,
+        en:"Cash",
+        ch:"现金"
+      },{
+        rk: 1,
+        en:"CCard",
+        ch:"信用卡"
+      },{
+        rk: 2,
+        en:"ThirdPay",
+        ch:"第三方支付"
+      },{
+        rk: 3,
+        en:"Other",
+        ch:"其他"
+      }];
 
   // loading动画，请求数据然后显示拆分支付方式
   function PayCheck(){
@@ -50,9 +67,14 @@ define(function(require, exports, module) {
           IsEnableTicketPay = payInfo.IsEnableTicketPay;
       tempData.total = _data.vars.initData.TotalAmount;
       tempData.Payments = payInfo.Payments;
+
       $.each(tempData.Payments, function(_index, _item){
-        var itemData = {};
+        var itemData = {},
+            paymentsIndex = tempData.Payments[_index];
         itemData.type = _item.PaymentType;
+        paymentsIndex['chid'] = '支付方式' + self._listNo(_item.PaymentId - 0);
+        paymentsIndex['chtype'] = self._typeFormat(_item.PaymentType);
+        // paymentsIndex['paylink'] = GVO.vars.handles.getPayHtmlInfo + '&PaymentId=' + _item.PaymentId;
         if( _item.PaymentType === 'Cash' ){
 
           // IsEnableTicketPay = false 并且是现金支付，不显示支付链接
@@ -74,22 +96,24 @@ define(function(require, exports, module) {
           // 第三方支付不用显示支付链接
           tempData.Payments[_index]['linkText'] = ''
         }
-//        switch (_item.PaymentType){
-//          case 'Cash':
-//            break;
-//          case 'CCard':
-//            break;
-//          case 'ThirdPay':
-//            break;
-//          case 'Other':
-//            break;
-//          default:
-//            break;
-//        }
-        console.log(_item)
       })
-      console.log(tempData);
+      return tempData;
     };
+
+    /**
+     * 英文支付方式转化为中文用于显示
+     * @param {String} _type 英文支付方式
+     * @returns {String}     中文支付方式
+     */
+    this._typeFormat = function(_type){
+      var tempType;
+      $.each(PAYTYPES, function(_index, _item){
+        if(_item.en === _type){
+          tempType = _item.ch
+        }
+      })
+      return tempType
+    }
 
     this._listNo = function( _num ){ // 转中文序号
       switch (_num){
@@ -133,37 +157,116 @@ define(function(require, exports, module) {
      *
      */
     this._tpl = function(){
-      return '{{#pay}}' +
+      return '{{#Payments}}' +
       '<li>' +
-        '<span class="pay_ways">{{NO}}</span>' +
-        '<span class="pay_choose">{{type}}</span>' +
-        '<p class="pay_yetnum">支付<span class="pay_price"><dfn>￥</dfn>{{money}}</span></p>' +
-        '{{#if giftcard}}' +
-        '<a href="javascript:void(0)" class="pay_link">填写礼品卡信息</a>' +
+        '<span class="pay_ways">{{chid}}：</span>' +
+        '<span class="pay_choose">{{chtype}}</span>' +
+        '<p class="pay_yetnum">支付<span class="pay_price"><dfn>￥</dfn>{{Amount}}</span></p>' +
+        '{{#if linkText}}' +
+        '<a href="javascript:void(0)" data-tag="{{PaymentId}}" class="pay_link">{{linkText}}</a>' +
         '{{/if}}' +
         '</li>' +
-        '{{/pay}}';
+        '{{/Payments}}';
     }
 
     /**
      * 拼装数据，给每个支付加上序号
      * @private
      */
-    this._payNo = function(){
-      $.each(data.pay, function(_index, _item){
-        data.pay[_index].NO = '支付方式' + self._listNo(_index + 1) + '：'
+//    this._payNo = function(){
+//      $.each(data.pay, function(_index, _item){
+//        data.pay[_index].NO = '支付方式' + self._listNo(_index + 1) + '：'
+//      })
+//    };
+
+    this._send = function(_args){
+      $.ajax({
+        type: 'post',
+        url: _args.url,
+        data: _args.data,
+        success: function(_data){
+          _args.callback(_data)
+        },
+        error: function(){
+          // _args.errback()
+        }
+      });
+    }
+
+    /**
+     * 检查BillNo是否有为0的
+     * @return {Boolean}
+     */
+    this._checkBillNo = function(_tplData){
+      var isZero = false;
+      $.each(_tplData, function(_index, _item){
+        if(_item.BillNo === "0"){
+          isZero = true
+        }
       })
+      return isZero
+    }
+
+    /**
+     * 绑定各种事件
+     *
+     */
+    this._bind = function(_tplData){
+      // 填写修改礼品卡信息
+      $('#J_paybox').delegate('.pay_link', 'click', function(){
+        self._send({
+          url: GVO.vars.handles.getPayHtmlInfo,
+          data: {
+            PaymentId: $(this).attr('data-tag')
+          },
+          callback: function(_data){
+            if(_data.errno === 0){
+              // 生成表单去提交
+              if($('#J_subform').length > 0){
+                $('#J_subform').remove()
+              }
+              $('body').append('<div style="display:none" id="J_subform">+ _data.data +</div>');
+
+              // 隐藏form提交
+              $('#J_subform').find('form').submit();
+            } else {
+              alert(_data.errmsg)
+            }
+          }
+        });
+      });
+
+      // 确认提交按钮
+      $('#J_submit').click(function(){
+        // IsDirectFlight = 0 && BillNo= 0 不能点提交链接
+        if(GVO.vars.IsDirectFlight === '0' && !self._checkBillNo(_tplData)){
+
+          // TODO 不能提交提示
+          return false;
+        } else {
+          self._send({
+            url:GVO.vars.SubmitOrderUrl,
+            data:{
+              saveorderinfo: GVO.vars.initData.paymentOrderInfoJson
+            },
+            callback: function(_data){
+
+            }
+          });
+        }
+      });
+
     };
-
     this.init = function(){
-
       // 重新格式化显示参数
       var tplData = this._formatData(GVO),
           template = Handlebars.compile(self._tpl());
-      this._payNo();
 
+      console.log(tplData);
       $('#J_loading').remove();
-      $("#J_paybox").html(template(data));
+      $("#J_paybox").html(template(tplData));
+
+      self._bind(tplData);
     }
   };
 
