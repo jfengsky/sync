bbf3fd96c34d582624a0022f9a830e6ea1cf4922
jfengsky <jfengsky@gmail.com>
2015-01-24 22:33:11 +0800

    youyou modify

---
+++ youyou/youyou.js
@@ -0,0 +1,21 @@
+/**
+ * @description: 游游助手脚本文件
+ * @author jiang.f@ctrip.com
+ * @time: 2015-01-24
+ */
+
+(function(window, undefined){
+  /**
+   * 考虑到移植性,不采用任何js框架, 直接用原生js
+   */
+  var QA = {};
+
+  // jsonp
+  QA.prototype.ajax = function(_url,_data,_callback){
+
+  };
+
+  // append
+  // html
+
+})(window);
