c302ce6a9e1989001f097ee236c468171a4bd4bc
jfengsky <jfengsky@gmail.com>
2015-01-25 20:53:21 +0800

    youyou modify

--- youyou/youyou.js
+++ youyou/youyou.js
@@ -4,15 +4,62 @@
  * @time: 2015-01-24
  */
 
-(function(window, undefined){
+(function(window, undefined) {
   /**
    * 考虑到移植性,不采用任何js框架, 直接用原生js
    */
-  var QA = {};
+  var QA = {
 
-  // jsonp
-  QA.prototype.ajax = function(_url,_data,_callback){
+    /**
+     * [$ description]
+     * @param  {[type]} _id [description]
+     * @return {[type]}     [description]
+     */
+    $: function(_id) {
+      return document.getElementById(_id)
+    },
 
+    /**
+     * [$Class description]
+     * @param  {[type]} id [description]
+     * @param  {[type]} el [description]
+     * @return {[type]}    [description]
+     */
+    $Class: function (_id, _el) {
+      var elements = document.getElementById(_id);
+      return elements.getElementsByTagName(_el);
+    },
+
+    /**
+     * 实现jsonp方法
+     * @param  {[type]} _options [description]
+     * @return {[type]}          [description]
+     */
+    jsonp: function(_options) {
+
+    },
+
+    /**
+     * [append description]
+     * @param  {[type]} _data [description]
+     * @return {[type]}       [description]
+     */
+    append: function(_data) {
+
+    },
+
+    /**
+     * [html description]
+     * @param  {[type]} _data [description]
+     * @return {[type]}       [description]
+     */
+    html: function(_data) {
+
+    },
+
+    bind: function() {
+
+    }
   };
 
   // append
