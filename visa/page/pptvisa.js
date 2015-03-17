1cc34b7b7f4fe7a5b6dfd42f86386bb43260a693
jfengsky <jfengsky@gmail.com>
2015-03-17 19:01:51 +0800

    visa modify

--- visa/page/pptvisa.js
+++ visa/page/pptvisa.js
@@ -14,12 +14,12 @@ function PptVisa_Page(_data) {
 
   $.map(_data.Pages[0].Values, function(_item) {
     if (_item.ColumnName === '护照/旅行证件号码' || _item.ColumnName === '护照本编号' || _item.ColumnName === '护照签发地—城市[英文]' || _item.ColumnName === '护照签发地—州/省份（如果护照上显示）[英文]' || _item.ColumnName === '签发日期-年' || _item.ColumnName === '失效日期-年') {
-      $('#' + _item.FormId).val(_item.Value);
-      $('#J_autowritetips').text(_item.ColumnName);
+      setVal(_item);
     };
 
     // 护照类型
     if (_item.ColumnName === '护照/旅行证件种类') {
+      tip(_item);
       if (_item.Value === 'T') {
 
         $.map($('#' + _item.FormId).find('option'), function(__optionItem) {
@@ -33,19 +33,17 @@ function PptVisa_Page(_data) {
         });
 
         var intervalPassportType = setInterval(function() {
-          $('#J_autowritetips').text('护照/旅行证件种类有异步操作,请稍后...');
+          tip(_item,2);
           if ($('#ctl00_SiteContentPlaceHolder_FormView1_tbxPptOtherExpl').length) {
             clearInterval(intervalPassportType);
-            $.map(_data.Pages[0].Values, function(_item) {
-              if (_item.ColumnName === '护照种类其他的说明[英文]') {
-                $('#' + _item.FormId).val(_item.Value);
-                passportTypeFinsh = true;
-              }
+            $.map(_data.Pages[0].Values, function(_secItem) {
+              setInputText('护照种类其他的说明[英文]', _secItem);
+              passportTypeFinsh = true;
             });
           }
         }, 1000);
       } else {
-        autoSelectValue('护照/旅行证件种类', _item);
+        setSelect('护照/旅行证件种类', _item);
         passportTypeFinsh = true;
       };
     }
@@ -61,40 +59,18 @@ function PptVisa_Page(_data) {
     autoSelectValue('护照签发地—国家/地区[英文]', _item);
 
     // 发放日期[日]
-    if (_item.ColumnName === '签发日期-日') {
-      $.map($('#' + _item.FormId).find('option'), function(__item) {
-        if ($(__item).attr('value') - 0 === _item.Value - 0) {
-          $(__item).prop('selected', true);
-        }
-      });
-    };
+    setSelect('签发日期-日', _item, 'number');
+    
 
     // 发放日期[月]
-    if (_item.ColumnName === '签发日期-月') {
-      $.map($('#' + _item.FormId).find('option'), function(__item) {
-        if (($(__item).attr('value') - 0) === (_item.Value - 0)) {
-          $(__item).prop('selected', true);
-        }
-      });
-    };
+    setSelect('签发日期-月', _item, 'number');
+
 
     // 过期日期[日]
-    if (_item.ColumnName === '失效日期-日') {
-      $.map($('#' + _item.FormId).find('option'), function(__item) {
-        if (($(__item).attr('value') - 0) === (_item.Value - 0)) {
-          $(__item).prop('selected', true);
-        }
-      });
-    };
+    setSelect('失效日期-日', _item, 'number');
 
     // 过期日期[月]
-    if (_item.ColumnName === '失效日期-月') {
-      $.map($('#' + _item.FormId).find('option'), function(__item) {
-        if (($(__item).attr('value') - 0) === (_item.Value - 0)) {
-          $(__item).prop('selected', true);
-        }
-      });
-    };
+    setSelect('失效日期-月', _item, 'number');
 
     // 过期不适用的
     autoNotApplyCheckbox('失效日期（不适用）', _item);
@@ -102,25 +78,22 @@ function PptVisa_Page(_data) {
     // 是否遗失过护照
     if (_item.ColumnName === '您的护照是否曾遗失或者被盗？') {
 
-      // 
       var clickLostPassport = setInterval(function() {
         if (passportTypeFinsh) {
           clearInterval(clickLostPassport);
           $('#' + _item.FormId).click();
         }
       }, 1000);
-      $('#J_autowritetips').text(_item.ColumnName);
+      tip(_item,1);
       if (_item.Value === 'True') {
         canShowNext = false;
         var intervallost = setInterval(function() {
-          $('#J_autowritetips').text('您的护照是否曾遗失或者被盗？有异步操作,请稍后...');
+          tip(_item,2);
           if ($('#ctl00_SiteContentPlaceHolder_FormView1_add1').length) {
             clearInterval(intervallost);
             $.map(_data.Pages[0].Values, function(_lostItem) {
-              if (_lostItem.ColumnName === '丢失的护照/旅行证件号码' || _lostItem.ColumnName === '丢失的说明[英文]') {
-                $('#' + _lostItem.FormId).val(_lostItem.Value);
-                $('#J_autowritetips').text(_lostItem.ColumnName);
-              };
+              setInputText('丢失的护照/旅行证件号码', _lostItem);
+              setInputText('丢失的说明[英文]', _lostItem);
 
               // 遗失护照号不适用的
               autoNotApplyCheckbox('丢失的护照/旅行证件号码（忘记）', _lostItem);
--- visa/page/travel.js
+++ visa/page/travel.js
@@ -28,15 +28,14 @@ function Travel_Page(_data) {
       if (tagName === 'INPUT') {
 
         if (tagType === 'text') {
-          writeStep(tagType, _item.ColumnName);
-
+          tip(_item);
           // text类型的表单直接写入值
           $inputDom.val(_item.Value);
         } else if (tagType === 'radio') {
 
           // 您是否已经制定了具体的旅行计划？
           if (_item.ColumnName === '您是否已经制定了具体的旅行计划？') {
-            $('#J_autowritetips').text('选择您是否已经制定了具体的旅行计划？');
+            tip(_item, 1);
             var planInterval = setInterval(function() {
               if (purposeHasSel) {
                 clearInterval(planInterval);
@@ -45,6 +44,7 @@ function Travel_Page(_data) {
             }, 1000);
             if (_item.Value === 'True') {
               var intervalPlanSel = setInterval(function() {
+                  tip(_item, 2);
                   if ($('#ctl00_SiteContentPlaceHolder_FormView1_specifictravel').length) {
                     clearInterval(intervalPlanSel);
                     $.map(_data.Pages[0].Values, function(__item) {
@@ -57,7 +57,7 @@ function Travel_Page(_data) {
                         tagTypeSec = $inputDomSec.attr('type');
                         if (tagNameSec === 'INPUT') {
                           if (tagTypeSec === 'text') {
-                            writeStep(tagTypeSec, __item.ColumnName);
+                            tip(__item);
                             $inputDomSec.val(__item.Value);
                           } else if (tagTypeSec === 'radio') {
 
@@ -66,20 +66,16 @@ function Travel_Page(_data) {
                           }
                         } else if (tagNameSec === 'SELECT') {
                           // 到达美国日期-日 || 到达美国日期-月 || 离开美国日期-日 || 离开美国日期-月
-                          if (__item.ColumnName === '到达美国日期-日' || __item.ColumnName === '到达美国日期-月' || __item.ColumnName === '离开美国日期-日' || __item.ColumnName === '离开美国日期-月') {
-                            $('#J_autowritetips').text(__item.ColumnName);
-                            $.map($('#' + __item.FormId).find('option'), function(_optionItem) {
-                              if ($(_optionItem).attr('value') - 0 === __item.Value - 0) {
-                                $(_optionItem).prop('selected', true);
-                              }
-                            });
-                          };
+                          setSelect('到达美国日期-日', __item, 'number');
+                          setSelect('到达美国日期-月', __item, 'number');
+                          setSelect('离开美国日期-日', __item, 'number');
+                          setSelect('离开美国日期-月', __item, 'number');
 
                           autoSelectValue('在美停留期间的住址-州[英文]', __item);
 
                           planHasSel = true;
                           if (__item.ColumnName === '支付您旅行费用的个人或组织名称') {
-                            writeStep(tagTypeSec, __item.ColumnName);
+                            tip(__item,1);
                             if (__item.Value === "O") {
                               $.map($('#' + __item.FormId).find('option'), function(__optionItem) {
                                 if ($(__optionItem).attr('value') === __item.Value) {
@@ -91,32 +87,30 @@ function Travel_Page(_data) {
                                 }
                               });
                               var intervalPaying = setInterval(function() {
+                                tip(__item,2);
                                 if ($('#ctl00_SiteContentPlaceHolder_FormView1_tbxPayerSurname').length) {
                                   clearInterval(intervalPaying);
                                   $.map(_data.Pages[0].Values, function(_thirdItem) {
                                     if (_thirdItem.ColumnName === '承担您旅行费用者的姓氏[英文]' || _thirdItem.ColumnName === '承担您旅行费用者的名字[英文]' || _thirdItem.ColumnName === '承担您旅行费用者的电话号码' || _thirdItem.ColumnName === '承担您旅行费用者的电子邮件地址') {
-                                      $('#' + _thirdItem.FormId).val(_thirdItem.Value);
-                                      $('#J_autowritetips').text(_thirdItem.ColumnName);
+                                      setVal(_thirdItem);
                                     }
 
                                     // TODO Does Not Ayyly 问题
                                     // autoNotApplyCheckbox('承担您旅行费用者与您关系[英文]', _item);
 
-                                    if (_thirdItem.ColumnName === '承担您旅行费用者与您关系[英文]') {
-                                      autoSelectValue('承担您旅行费用者与您关系[英文]', _thirdItem);
-                                    }
+                                    setSelect('承担您旅行费用者与您关系[英文]', _thirdItem);
+                                    
 
                                     if (_thirdItem.ColumnName === '承担您旅行费用者地址是否与您家庭或右击地址相同') {
                                       $('#' + _thirdItem.FormId).click()
                                       if (_thirdItem.Value === 'False') {
-                                        writeStep('loading', _thirdItem);
                                         var intervalSame = setInterval(function() {
+                                          tip(_thirdItem,2);
                                           if ($('#ctl00_SiteContentPlaceHolder_FormView1_tbxPayerStreetAddress1').length) {
                                             clearInterval(intervalSame);
                                             $.map(_data.Pages[0].Values, function(_forthItem) {
                                               if (_forthItem.ColumnName === '承担您旅行费用者街道地址（第一行）[英文]' || _forthItem.ColumnName === '承担您旅行费用者街道地址（第二行）[英文]' || _forthItem.ColumnName === '承担您旅行费用城市[英文]' || _forthItem.ColumnName === '承担您旅行费用州/省份[英文]' || _forthItem.ColumnName === '承担您旅行费用邮政编码') {
-                                                $('#' + _forthItem.FormId).val(_forthItem.Value);
-                                                $('#J_autowritetips').text(_forthItem.ColumnName);
+                                                setVal(_forthItem);
                                               };
 
                                               autoNotApplyCheckbox('承担您旅行费用州/省份不适用的', _forthItem);
@@ -151,8 +145,7 @@ function Travel_Page(_data) {
                                   clearInterval(intervalPayingC);
                                   $.map(_data.Pages[0].Values, function(_thirdItem) {
                                     if (_thirdItem.ColumnName === '承担您旅行费用的公司或组织名称[英文]' || _thirdItem.ColumnName === '承担您旅行费用的公司或组织名称电话号码' || _thirdItem.ColumnName === '承担您旅行费用的公司或组织名称与您的关系[英文]' || _thirdItem.ColumnName === '承担您旅行费用的公司或组织名称街道地址（第一行）[英文]' || _thirdItem.ColumnName === '承担您旅行费用的公司或组织名称街道地址（第二行）[英文]' || _thirdItem.ColumnName === '承担您旅行费用的公司或组织名称城市[英文]' || _thirdItem.ColumnName === '承担您旅行费用的公司或组织名称州/省份[英文]' || _thirdItem.ColumnName === '承担您旅行费用的公司或组织名称邮政编码') {
-                                      $('#' + _thirdItem.FormId).val(_thirdItem.Value);
-                                      $('#J_autowritetips').text(_thirdItem.ColumnName);
+                                      setVal(_thirdItem);
                                     }
                                     autoNotApplyCheckbox('承担您旅行费用的公司或组织名称州/省份不适用的', _thirdItem);
 
@@ -190,19 +183,18 @@ function Travel_Page(_data) {
               planHasSel = true
             }
           } else {
-            writeStep(tagType, _item.ColumnName);
+            tip(_item,1);
             // radio类型的选中
             $inputDom.click();
           };
 
         } else if (tagType === 'checkbox') {
-          writeStep(tagType, _item.ColumnName);
+          tip(_item,1);
         }
 
       } else if (tagName === 'SELECT') {
         // 赴美访问目的
         if (_item.ColumnName === '赴美访问目的') {
-          $('#J_autowritetips').text('赴美访问目的有异步操作,请稍后...');
           $.map($('#' + _item.FormId).find('option'), function(__optionItem) {
             if ($(__optionItem).attr('value') === _item.Value) {
               // js去触发select的change事件
@@ -213,9 +205,9 @@ function Travel_Page(_data) {
             }
           });
           var intervalPurpose = setInterval(function() {
+            tip(_item,2);
             if ($('#ctl00_SiteContentPlaceHolder_FormView1_dlPrincipalAppTravel_ctl00_ddlOtherPurpose').length) {
               clearInterval(intervalPurpose);
-              $('#J_autowritetips').text('选择赴美访问目的具体说明');
               $.map(_data.Pages[0].Values, function(__item) {
                 autoSelectValue('赴美访问目的具体说明', __item);
                 purposeHasSel = true;
