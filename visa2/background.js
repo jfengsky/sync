f1d4faa9ad1136f8a4ed5584b0a0d005b3daeebb
姜峰 <jiang.f@ctrip.com>
2015-07-13 10:08:35 +0800

    visa2 modify

--- visa2/background.js
+++ visa2/background.js
@@ -27,7 +27,7 @@ chrome.extension.onMessage.addListener(function(objRequest, _, sendResponse) {
     $.ajax({
       // url: 'http://order.visa.ctripcorp.com/Visa-Order-OrderProcess/VisaAutoComplete/VisaAutoCompleteApi.aspx',
       url: 'http://order.visa.fat6.qa.nt.ctripcorp.com/Visa-Order-OrderProcess/VisaAutoComplete/VisaAutoCompleteApi.aspx',
-      // url: 'http://localhost:3001/VisaAutoCompleteApi',
+       // url: 'http://localhost:3001/VisaAutoCompleteApi',
       type: 'get',
       data: objRequest.sendParam,
       dataType: 'json',
