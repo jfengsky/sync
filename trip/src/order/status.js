7d275943dccf5518e93770cec2e048f6f7471b4f
姜峰 <jiang.f@ctrip.com>
2015-09-24 13:29:20 +0800

    delete trips

--- trip/src/order/status.js
+++
@@ -1,8 +0,0 @@
-define(function(require, exports, module){
-	return {
-		error: {},
-		isTmpSave: false,
-		isPay: false,
-		errorElem: null
-	};
-});
--- trip/src/tpl/Modules/CalendarBig/MonthCtrl.html.js
+++
@@ -1,9 +0,0 @@
-define(function(){
-	return "<p>\n\
-	{{#is MonthCount 2}}\n\
-	<span class=\"border {{#is FirstMonthLineCount 2}}two{{else}}three{{/is}}_line\">{{#is FirstMonthLineCount \'>\' 2}}{{YearInMaxMonth}}年<br>{{/is}}{{MonthFirst}}月</span>\n\
-	<span class=\"{{#is FirstMonthLineCount 2}}three{{else}}two{{/is}}_line\">{{#is FirstMonthLineCount \'<=\' 2}}{{YearInMaxMonth}}年<br>{{/is}}{{MonthSecond}}月</span>\n\
-	{{else}}\n\
-	<span>{{Year}}年<br>{{Month}}月</span>\n\
-	{{/is}}\n\
-</p>"});
