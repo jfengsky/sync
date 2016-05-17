ffb908c36f768148e67226b6c42b92b4cd2e87e9
姜峰 <jiang.f@ctrip.com>
2016-05-17 17:32:37 +0800

    add objectes6

--- babel/objectes6.js
+++ babel/objectes6.js
@@ -1,3 +1,5 @@
-/**
- * Created by jiang.f on 2016/5/17.
- */
+let target = {a: 1,e:[333,444]}
+let source1 = {b: 2}
+let source2 = {c: 3}
+Object.assign(target, source1, source2)
+console.log(target)
