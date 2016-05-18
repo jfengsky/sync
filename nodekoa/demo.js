4700e1a0509372748fe693016beefa9e293ccaf4
姜峰 <jiang.f@ctrip.com>
2016-05-18 09:29:20 +0800

    koa

--- nodekoa/demo.js
+++ nodekoa/demo.js
@@ -1,9 +1,25 @@
 var koa = require('koa');
+var route = require('koa-route');
 var app = koa();
+ 
+app.use(route.get('/api/items', function *(){
+  this.body = 'Get'
+}));
+
+app.use(route.get('/api/items/:id', function *(id){
+  this.body = 'Get id:' + id
+}));
+
+app.use(route.post('/api/items', function *(){
+  this.body = 'post'
+}));
+
+app.use(route.put('/api/items/:id', function *(){
+  this.body = 'put id:' + id
+}));
 
 app.use(function *(){
-  var path = this.path;
-  this.body = path;
-})
-
+  this.body = 'Hello World';
+});
+ 
 app.listen(3010);
