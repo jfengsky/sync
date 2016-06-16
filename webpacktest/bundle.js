1fe96dd3702a6eb368b212c7e3c32495dfe5533f
姜峰 <jiang.f@ctrip.com>
2016-06-16 09:20:02 +0800

    webpack test

---
+++ webpacktest/bundle.js
@@ -0,0 +1,17 @@
+/*! This file is created by zhaoda */
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _jquery = __webpack_require__(1);
+
+	var _jquery2 = _interopRequireDefault(_jquery);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	console.log(_jquery2.default);
+
+/***/ }
+]);
